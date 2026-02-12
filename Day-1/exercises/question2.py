f651fb461bc1fc1f949e6185e33fc62d984c7d8b
Aditi Pokharel <aditipokharel09@gmail.com>
2026-02-12 22:26:55 +0545

    Day 1: Euclidean distance and data type exercises

---
+++ Day-1/exercises/question2.py
@@ -0,0 +1,2 @@
+message = "My name is Aditi Pokharel. I am from Nepal. I am enjoying my python journey."
+print(message)
---
+++ Day-1/variables_and_types.py
@@ -0,0 +1,18 @@
+name = "aditi"
+print(name)
+
+print(type(name) == str)
+print(isinstance(name, str))
+
+age = float(2)
+print(isinstance(age, float))
+
+number = "20"
+age = int(number)
+print(isinstance(age, int))
+
+print("kancho" + " is a good dog")
+
+age = 10
+age += 10
+print(age)
