89d5aeab0e64278d40ff51546d2d790d9dab7012
Aditi Pokharel <aditipokharel09@gmail.com>
2026-02-22 22:06:59 +0545

    Day 5: Strings and Debugging

---
+++ Day-5/exercises/practice2.py
@@ -0,0 +1,12 @@
+company = "Coding For All"
+
+# To Uppercase
+print(company.upper())
+
+# To Lowercase
+print(company.lower())
+
+# Formatting methods
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
