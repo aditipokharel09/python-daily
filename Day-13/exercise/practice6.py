1f0c7ef1fc5ea4e8f0cdd6ca5c4b2fa8eb89abfe
Aditi Pokharel <aditipokharel09@gmail.com>
2026-02-26 21:06:16 +0545

    Day-13: Errors part 1

---
+++ Day-13/exercise/practice6.py
@@ -0,0 +1,18 @@
+KeyError
+
+"""PS C:\Users\Acer\OneDrive\Desktop\Mastery journey\python daily\Day-13> python
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+Ctrl click to launch VS Code Native REPL
+>>> users = {'name':'Aditi', 'age':21, 'country':'Nepal'}  
+>>> users['name']
+'Aditi'
+>>> users['county']
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+    import platform
+KeyError: 'county'
+>>> users['country']
+'Nepal'
+>>> 
+"""
---
+++ Day-13/exercise/practice7.py
@@ -0,0 +1,18 @@
+TypeError
+
+"""PS C:\Users\Acer\OneDrive\Desktop\Mastery journey\python daily\Day-13> python
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+Ctrl click to launch VS Code Native REPL
+>>> 4 + '3'
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+    import platform
+    ^^^^^^^
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 4 + int('3')
+7
+>>> 4 + float('3')
+7.0
+>>> 
+"""
